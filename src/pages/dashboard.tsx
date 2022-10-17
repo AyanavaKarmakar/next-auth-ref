fd7d3d10c5e6107629bdcdc7ce8e3406b880ecc6
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-10-17 22:07:21 +0530

    feat: fixed restricted access

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,26 @@
 import type { NextPage } from "next";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 import { NavbarComponent } from "../components";
 
 const Dashboard: NextPage = () => {
+  const { status } = useSession();
+  const router = useRouter();
+
+  /**
+   * ? Runs when auth is running
+   */
+  if (status === "loading") {
+    return null;
+  }
+
+  /**
+   * ? Prevents access if not logged in
+   */
+  if (status === "unauthenticated") {
+    router.push("/");
+  }
+
   return (
     <>
       <header>
