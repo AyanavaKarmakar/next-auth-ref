0e46b5d78933675d916320d88a39986260a8aa53
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-10-17 16:09:15 +0530

    feat: create Navbar component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,12 @@
 import type { NextPage } from "next";
+import { NavbarComponent } from "../components";
 
 const Home: NextPage = () => {
   return (
     <>
+      <header>
+        <NavbarComponent />
+      </header>
       <main>
         <article className="prose">
           <h1>Home Page</h1>
