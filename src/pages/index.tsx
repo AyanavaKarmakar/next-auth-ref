803e3f5e11720996d88c5050e61afecb86a0a9f3
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-10-18 00:33:09 +0530

    feat: set loading display when auth session is running

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,18 @@
 import type { NextPage } from "next";
+import { useSession } from "next-auth/react";
 import { NavbarComponent } from "../components";
 
 const Home: NextPage = () => {
+  const { status } = useSession();
+
+  if (status === "loading") {
+    return (
+      <article className="prose">
+        <h1>Loading</h1>
+      </article>
+    );
+  }
+
   return (
     <>
       <header>
